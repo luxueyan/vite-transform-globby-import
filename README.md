37fa0a2b2cb702f179c43cbf0c97241afe122d26
luxueyan <luxueyan@kaitongamc.com>
2020-08-14 17:29:06 +0800

    feat: support more import syntax

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 
 This transform is an simple resolution for enchancement of 'import statement'. It just replace the globby 'import statement' with multiple import lines before the default transforms.
 
-
 ## Status
 
 In rc.1 and will likely release 1.0 soon.
@@ -22,34 +21,66 @@ In rc.1 and will likely release 1.0 soon.
 // vite.config.js
 const sharedConfig = {
   alias: {
-    '/@/': path.resolve(__dirname, 'src'),
-  },
+    '/@/': path.resolve(__dirname, 'src')
+  }
 }
 module.exports = {
   ...sharedConfig,
-  transforms: [require('vite-transform-globby-import')(sharedConfig)],
+  transforms: [require('vite-transform-globby-import')(sharedConfig)]
 }
 ```
 
-Example:
+Example: 
+
 ```ts
 import routes from 'globby!../pages/**/route.ts'
-import imgs from 'globby!/@/assets/image/**/*.@(jpg|png)'
-// These will be replaced to:
-/* 
+// will be replaced to 
+/*
+ * import routes0 from '/@/pages/route.ts'
+ * import routes1 from '/@/pages/demo/route.ts'
+ * ...
+ * const routes = { routes0, routes1, ... }
+ *
+ */
+import * as routes from 'globby!../pages/**/route.ts'
+// will be replaced to 
+/*
  * import * as routes0 from '/@/pages/route.ts'
  * import * as routes1 from '/@/pages/demo/route.ts'
  * ...
  * const routes = { routes0, routes1, ... }
- * import * as imgs0 from '/@/assets/image/demo.jpg'
- * import * as imgs1 from '/@/assets/image/demo/demo.png'
+ *
+ */
+import { route } from 'globby!../pages/**/route.ts'
+// will be replaced to 
+/*
+ * import { route as route0 } from '/@/pages/route.ts'
+ * import { route as route1 } from '/@/pages/demo/route.ts'
+ * ...
+ * const route = { route0, route1, ... }
+ *
+ */
+
+import { route as demo } from 'globby!../pages/**/route.ts'
+// will be replaced to 
+/*
+ * import { route as demo0 } from '/@/pages/route.ts'
+ * import { route as demo1 } from '/@/pages/demo/route.ts'
+ * ...
+ * const demo = { demo0, demo1, ... }
+ *
+ */
+import imgs from 'globby!/@/assets/image/**/*.@(jpg|png)'
+// These will be replaced to:
+/*
+ * import imgs0 from '/@/assets/image/demo.jpg'
+ * import imgs1 from '/@/assets/image/demo/demo.png'
  * ...
  * const imgs = { imgs0, imgs1, ... }
  */
 ```
 
-**Note:** Only work in files includes `.vue,.js,.jsx,.ts,.tsx`. 
-
+**Note:** Only work in files includes `.vue,.js,.jsx,.ts,.tsx`.
 
 ## License
 
@@ -59,6 +90,7 @@ MIT
 [npm-url]: https://npmjs.com/package/vite-transform-globby-import
 [node-img]: https://img.shields.io/node/v/vite.svg
 [node-url]: https://nodejs.org/en/about/releases/
+
 <!-- [unix-ci-img]: https://circleci.com/gh/vitejs/vite.svg?style=shield
 [unix-ci-url]: https://app.circleci.com/pipelines/github/vitejs/vite
 [windows-ci-img]: https://ci.appveyor.com/api/projects/status/0q4j8062olbcs71l/branch/master?svg=true
--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,19 @@ interface SharedConfig {
   resolvers?: Resolver[]
 }
 
+function template(template: string) {
+  return (data: { [x: string]: any }) => {
+    return template.replace(/#([^#]+)#/g, (_, g1) => data[g1] || g1)
+  }
+}
+
 const globbyTransfrom = function (config: SharedConfig): Transform {
   const resolver = createResolver(
     config.root || process.cwd(),
     config.resolvers || [],
     config.alias || {}
   )
+  const cache = new Map()
   return {
     test({ path }) {
       const filePath = path.replace('\u0000', '') // why some path startsWith '\u0000'?
@@ -31,31 +38,50 @@ const globbyTransfrom = function (config: SharedConfig): Transform {
         return false
       }
     },
-    transform({ code, path }) {
-      return code.replace(
-        /import\s+(\w+)\s+from\s+(['"])globby!([^'"]+)\2/g,
-        (_, g1, g2, g3) => {
-          const filePath = path.replace('\u0000', '') // why some path startsWith '\u0000'?
-          const resolvedFilePath = g3.startsWith('.')
-            ? resolver.resolveRelativeRequest(filePath, g3)
-            : { pathname: resolver.requestToFile(g3) }
-          const files = glob.sync(resolvedFilePath.pathname, { dot: true })
+    transform({ code, path, isBuild }) {
+      let result = cache.get(path)
+      if (!result) {
+        result = code.replace(
+          /import\s+([\w\s{}\*]+)\s+from\s+(['"])globby!([^'"]+)\2/g,
+          (_, g1, g2, g3) => {
+            const filePath = path.replace('\u0000', '') // why some path startsWith '\u0000'?
+            // resolve path
+            const resolvedFilePath = g3.startsWith('.')
+              ? resolver.resolveRelativeRequest(filePath, g3)
+              : { pathname: resolver.requestToFile(g3) }
+            const files = glob.sync(resolvedFilePath.pathname, { dot: true })
 
-          let groups: string[] = []
-          let replaceFiles = files.map((f, i) => {
-            groups.push(g1 + i)
-            return `import * as ${g1 + i} from ${g2}${resolver.fileToRequest(
-              f
-            )}${g2}`
-          })
-          return (
-            replaceFiles.join('\n') +
-            `\nconst ${g1} = { ${groups.join(', ')} }\n`
-          )
-        }
-      )
-      // console.log(ctx.code)
-      // return ctx
+            let templateStr = 'import #name# from #file#' // import default
+            let name = g1
+            const m = g1.match(/\{\s*(\w+)(\s+as\s+(\w+))?\s*\}/) // import module
+            const m2 = g1.match(/\*\s+as\s+(\w+)/) // import * as all module
+            if (m) {
+              templateStr = `import { ${m[1]} as #name# } from #file#`
+              name = m[3] || m[1]
+            } else if (m2) {
+              templateStr = 'import * as #name# from #file#'
+              name = m2[1]
+            }
+            const temRender = template(templateStr)
+
+            const groups: Array<string>[] = []
+            const replaceFiles = files.map((f, i) => {
+              const file = g2 + resolver.fileToRequest(f) + g2
+              groups.push([name + i, file])
+              return temRender({ name: name + i, file })
+            })
+
+            return (
+              replaceFiles.join('\n') +
+              // '\n' +
+              // groups.map((v) => `${v[0]}._path = ${v[1]}`).join('\n') +
+              `\nconst ${name} = { ${groups.map((v) => v[0]).join(',')} }\n`
+            )
+          }
+        )
+        if (isBuild) cache.set('path', result)
+      }
+      return result
     }
   }
 }
